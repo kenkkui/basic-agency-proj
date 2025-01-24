57713346e5aa8d3637aeabba134c8a0a8105c9d0
kenkkui <kenzlain@gmail.com>
2025-01-24 16:13:20 +0800

    add loading heading animation

--- components/Loading/Loading.tsx
+++ components/Loading/Loading.tsx
@@ -1,15 +1,10 @@
-import clsx from "clsx";
 import LoadingHeadingText from "./LoadingHeadingText";
 import LoadingHeadingAnimation from "./LoadingHeadingAnimation";
 
 const Loading = () => {
-  // 1. make invisible none movable skeleton heading in the center of the screen
-  // 2. make it twice to show the effect of the heading in the center using absolute position
-  // 3.
-
   return (
     <section className="w-screen h-screen bg-white flex justify-center items-center">
-      <div className="bg-yellow-200 relative text-[128px] leading-[0.9] uppercase font-black tracking-tight">
+      <div className=" relative text-[128px] leading-[0.9] uppercase font-black tracking-tight">
         <LoadingHeadingText hide />
 
         <LoadingHeadingAnimation>
--- components/Loading/LoadingHeadingAnimation.tsx
+++ components/Loading/LoadingHeadingAnimation.tsx
@@ -1,3 +1,6 @@
+"use client";
+
+import parseEasing from "@/utils/parseEasing";
 import { m } from "framer-motion";
 
 interface LoadingHeadingAnimationProps {
@@ -9,10 +12,14 @@ const LoadingHeadingAnimation = ({
 }: LoadingHeadingAnimationProps) => {
   return (
     <m.div
-      className="absolute top-0 left-0 bg-red-400 overflow-hidden"
+      className="absolute top-0 left-0 overflow-hidden"
       initial={{ height: "100%" }}
       animate={{ height: 0 }}
-      transition={{ duration: 0.2, delay: 0.2 }}
+      transition={{
+        duration: 0.62,
+        delay: 0.2,
+        ease: parseEasing("expo.inOut"),
+      }}
     >
       {children}
     </m.div>
